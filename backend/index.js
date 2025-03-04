9c2d778d55d6684815a236d3fd1cd8ed20131fb9
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-03-04 18:10:32 +0300

    Init

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Our app is running on port ${PORT}`);
+})
+
+const bcrypt =require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+app.post('/register',async (req, res)=>{
+    const{name,email,password}= req.body;
+
+    if (!email || !password){
+        return res.status(400).json({error:"Your email and password are required to register"});
+    }
+   
+});

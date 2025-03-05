18bb274883a7cac55392ba3866af2da80e09bd31
Lusingalisa <lusingalisa7@gmail.com>
2025-03-05 12:01:10 +0300

    chapter

---
+++ backend/Capstone_backend/models/chapters.js
@@ -0,0 +1,9 @@
+const sqlChapters = `CREATE TABLE IF NOT EXISTS chapters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT
+)`;
+db.query(sqlChapters, (error) => {
+    if (error) throw error;
+    console.log("✅ Chapters table ready");
+});

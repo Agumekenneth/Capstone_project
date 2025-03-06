9b39b46743a484f4725a309c38bfb25fa5c8a48d
Lusingalisa <lusingalisa7@gmail.com>
2025-03-06 17:14:21 +0300

    index edits

--- backend/Capstone_backend/models/config.js
+++ backend/Capstone_backend/models/config.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "your_username", // replace with your MySQL username
-    password: "joshua@54", // replace with your MySQL password
-    database: "CSE_ENGAGEMENT"
+    user: "root", // replace with your MySQL username
+    password: "Lusinga@2003", // replace with your MySQL password
+    database: "CSE_PLATFORM"
 });
 
 // Connect to MySQL

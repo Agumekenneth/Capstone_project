c4758049028068704fb26e9733b5f553b47596d1
Lusingalisa <lusingalisa7@gmail.com>
2025-03-09 22:50:44 +0300

    moving things to the index file

--- backend/Capstone_backend/MYSQL/config.js
+++ backend/Capstone_backend/MYSQL/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root", // replace with your MySQL username
-    password: "joshua@54", // replace with your MySQL password
+    password: "Lusinga@2003", // replace with your MySQL password
     database: "CSE_PLATFORM"
 });
 
